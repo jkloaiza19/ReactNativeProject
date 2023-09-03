c4e4ac0429ef39e725fc3f88e35c770db3de019c
jkloaiza19 <58517868+jkloaiza19@users.noreply.github.com>
2023-09-03 09:21:49 -0600

    Add Navigattion stack (#2)

---
+++ src/components/NavigationTabBar/NavigationTabBar 2.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export const NavigationTabBar = ():React.ReactNode => (
+  <View style={{ flex: 1, width: '100%', height: '100%', backgroundColor: '#000' }} />
+)
