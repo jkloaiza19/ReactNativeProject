2eb8eb31d88e01d339ee46b6d8d0a55aa39b599d
Juan Carlos <jkloaiza19@gmail.com>
2023-09-08 02:14:01 -0600

    Add AppCenter config

--- ios/ReactNativeProject/AppDelegate.mm
+++ ios/ReactNativeProject/AppDelegate.mm
@@ -2,6 +2,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <AppCenterReactNativeShared/AppCenterReactNativeShared.h>
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -10,6 +15,12 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  [AppCenterReactNativeShared setStartAutomatically:YES];
+  [AppCenterReactNativeShared setAppSecret:@"ba09a92f-198d-409e-9238-3f7abdbbc9dd"];
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
